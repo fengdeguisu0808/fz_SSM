86272201012a9cd712d706575cba388ee2fba8b3
fengdeguisu <fanzhou_0808@163.com>
2020-07-09 23:14:32 +0800

    submit MybatisTest for fz_ssm

--- src/main/java/com/fanzhou/dao/UserDao.java
+++ src/main/java/com/fanzhou/dao/UserDao.java
@@ -1,6 +1,17 @@
 package com.fanzhou.dao;
 
+import com.fanzhou.domain.User;
+
+import java.util.List;
+
+/**
+ * @author fengdeguisu0808
+ * @date 2020/7/9 22:46
+ */
 public interface UserDao {
-
-
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<User> findAll();
 }
---
+++ src/test/java/com/fanzhou/test/MybatisTest.java
@@ -0,0 +1,29 @@
+package com.fanzhou.test;
+
+import com.fanzhou.dao.UserDao;
+import com.fanzhou.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author fengdeguisu0808
+ * @date 2020/7/9 23:04
+ */
+public class MybatisTest {
+    public static void main(String[] args) throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sessionFactory = builder.build(resourceAsStream);
+        SqlSession sqlSession = sessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> users = userDao.findAll();
+        users.forEach((user) -> System.out.println(user));
+
+    }
+}
